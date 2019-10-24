d5675e3bfed0d737fd580ddec028ad68e8f684af
MaiiaPonomarenko <nedopyokish@gmail.com>
2019-10-24 23:22:47 +0500

    Бонусное №1

---
+++ bonus01.js
@@ -0,0 +1,14 @@
+let check = function () {
+  let str = +prompt("Введите число");
+  let reg = new RegExp("[\p{Alpha}\p{M}\p{Pc}\p{Join_C}]");
+  console.log(reg.test(str));
+  
+  while (reg.test(str) || str === ''){
+    str = prompt("Вы ввели неверное значение. Попробуйте еще раз");
+  } return str;
+};
+
+let a = check();
+let b = check();
+
+a > b ? alert(a + " > " + b) : a < b ? alert(b + " > " + a) : alert(a + " = " + b);
