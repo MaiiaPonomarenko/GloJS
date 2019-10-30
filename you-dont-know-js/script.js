c495a69601155190122084185c4fb63a48f493ef
MaiiaPonomarenko <nedopyokish@gmail.com>
2019-10-30 22:31:21 +0500

    Первое обязательное. Добавлен последний пункт.

--- you-dont-know-js/script.js
+++ you-dont-know-js/script.js
@@ -23,7 +23,8 @@ commercial.classList.remove('adv');
 //5. восстановить порядок глав во второй и пятой книге
 let ul = document.querySelectorAll('ul'),
   li2 = ul[1].querySelectorAll('li'),
-  li5 = ul[4].querySelectorAll('li');
+  li5 = ul[4].querySelectorAll('li'),
+  li6 = ul[5].querySelectorAll('li');
 
 ul[1].insertBefore(li2[2], li2[10]);
 ul[1].insertBefore(li2[6], li2[4]);
@@ -32,6 +33,13 @@ ul[1].insertBefore(li2[8], li2[4]);
 ul[4].insertBefore(li5[9], li5[2]);
 ul[4].insertBefore(li5[5], li5[8]);
 ul[4].insertBefore(li5[2], li5[6]);
+
+//6. В шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место
+let liG6 = document.createElement('li');
+liG6.textContent = 'Глава 8: За пределами ES6';
+liG6 = ul[5].appendChild(liG6);
+ul[5].insertBefore(liG6, li6[9]);
+console.log(ul[2]);
 
 // для красоты
 const h1 = document.querySelector('h1');
