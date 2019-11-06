d04d1c5b69813c8b25c78f38f32d414d39910b4a
MaiiaPonomarenko <nedopyokish@gmail.com>
2019-11-06 12:30:22 +0500

    Добавлено бонусное для практики.

---
+++ js/bonus.js
@@ -0,0 +1,18 @@
+/**
+ * Created by User on 06.11.2019.
+ */
+const cityArr = {
+  rus: ['Москва', 'Санк-Петербург', 'Новосибирск', 'Екатеринбург', 'Нижний Новгород', 'Казань', 'Челябинск'],
+  uk: ['Киев', 'Харьков', 'Одесса', 'Днепр', 'Донецк', 'Запорожье', 'Львов'],
+  bel: ['Минск', 'Гомель', 'Могилёв', 'Витебск', 'Гродно', 'Брест'],
+  jap: ['Токио', 'Киото', 'Осака', 'Иокогама']
+};
+
+const country = document.querySelector('[name=select_5]');
+let options = country.querySelectorAll('option');
+
+const city = document.getElementById('city');
+
+
+
+console.log(option);
